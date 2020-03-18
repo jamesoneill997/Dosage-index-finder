ce37688d2c8be8051ccb3c4ce9a3246e7407bdae
jamesoneill997 <jamesoneill997@gmail.com>
2020-03-18 03:51:56 +0000

    results format with relevant info and write to file

--- di.py
+++ di.py
@@ -13,24 +13,27 @@ chrome_options.add_argument("--headless")
 browser = webdriver.Chrome("../../../chromedriver", chrome_options = chrome_options)
 
 #enter horse names into the below list in quotes, separated by commas. eg. ["red rum","arkle"]
-horses = []
+horses = ["new year new me"]
+f = open("output.txt", 'w+')
 
 class Horse_tipper():
 	def get_dosage(self,horses):
+		results = []
 		browser.get("https://www.pedigreequery.com/")
 		for horse in horses:
 			browser.find_element_by_xpath("/html/body/table/tbody/tr[2]/td/form/table/tbody/tr/td[1]/input").send_keys(horse)
 
 			try:
 				browser.find_element_by_xpath("/html/body/table/tbody/tr[2]/td/form/table/tbody/tr/td[5]/input").click()
-				details = browser.find_element_by_xpath("/html/body/table/tbody/tr[5]/td[1]/center/table[1]/tbody/tr/td[1]/center/font").text
+				details = browser.find_element_by_xpath("/html/body/table/tbody/tr[5]/td[1]/center/table[1]/tbody").text
+				print(details)
 				
-				print(horse, details[details.rfind("DI"):])
+				results.append(horse + " " + details[details.rfind("DI"):])
 			
 			except:
 				options = browser.find_element_by_xpath("/html/body/table/tbody/tr[5]/td[1]/blockquote/table/tbody").text
 				print(options)
-				horse_num = input("What is the index of the horse")
+				horse_num = input("What is the index of the horse? ")
 
 				if int(horse_num) > 1:
 	
@@ -42,7 +45,15 @@ class Horse_tipper():
 					details = browser.find_element_by_xpath("/html/body/table/tbody/tr[5]/td[1]/center/table[1]/tbody/tr/td[1]/center/font").text
 
 				
-				print(horse, details[details.rfind("DI"):])
+				results.append(horse + " " + details[details.rfind("DI"):])
+		
+		
+		for result in results:
+			print(results)
+			f.write(result)
+
+
 	
 tip = Horse_tipper()
-print(tip.get_dosage(horses))
+
+tip.get_dosage(horses)
